03b193a0ccc4cfd1727c8796c672dc1e149746a3
kannikakr <kannikashivu18@gmail.com>
2024-10-06 21:48:12 +0530

    added 2nd question code

---
+++ Pal_num/pal_num.cpp/pal_num.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if (x < 0) return false; 
+        int a = x;
+        int rem = 0, rev = 0;
+        
+        while (x != 0) {
+            rem = x % 10;
+            if (rev > (INT_MAX - rem) / 10) {
+                return false; 
+            }
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+        
+        return a == rev;
+    }
+};
