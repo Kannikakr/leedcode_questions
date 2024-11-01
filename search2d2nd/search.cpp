0b8fbf9a1659e9b7f60e593219aaa3b7e91bad84
kannikakr <kannikashivu18@gmail.com>
2024-11-01 23:07:26 +0530

    updated 20th solution

--- search2d2nd/search.cpp
+++ search2d2nd/search.cpp
@@ -3,18 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        
-        // Start from the top-right corner
+
         int row = 0, col = cols - 1;
         
         while (row < rows && col >= 0) {
             if (matrix[row][col] == target) {
                 return true;
             } else if (matrix[row][col] > target) {
-                // Move left
                 col--;
             } else {
-                // Move down
                 row++;
             }
         }
